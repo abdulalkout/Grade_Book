060bb4c7d14e6554a57de9c6db9326704f50748f
abdulalkout <abdul.alkout@gmail.com>
2023-11-21 13:24:57 -0600

    First commit Project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import studentsGrades from "./modules/students";
+import { useRef, useState } from "react";
 
 function App() {
+  const studentName = useRef(null);
+  const [student, setStudent] = useState("");
+
+  const studentSearch = (e) => {
+    e.preventDefault();
+    let newStudent = studentName.current.value.toLowerCase();
+    setStudent(newStudent);
+    // console.log(student);
+  };
+
+  let searchedStudent = studentsGrades.filter((currentStudent) => {
+    return currentStudent.name.toLowerCase() === student;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={studentSearch} className="App">
+        <h2>Grades</h2>
+
+        <input type="text" ref={studentName} />
+        <input type="submit" value="Submit Form" />
+      </form>
+      {searchedStudent.length > 0 ? (
+        <div style={{ marginLeft: "20px" }}>
+          <h4>Name : {searchedStudent[0].name}</h4>
+          <h4>GPA : {searchedStudent[0].GPA}</h4>
+          {searchedStudent[0].classes.map((classItem, index) => (
+            <h4 key={index}>
+              Class{index + 1} : {classItem}
+            </h4>
+          ))}
+        </div>
+      ) : (
+        <h4 style={{ textAlign: "center" }}>
+          Name was not found in the database
+        </h4>
+      )}
     </div>
   );
 }
---
+++ src/modules/students.jsx
@@ -0,0 +1,11 @@
+const studentsGrades = [
+  { name: "Jhon", GPA: 3.5, classes: ["HTML", "CSS", "JavaScript", "React"] },
+  { name: "Jack", GPA: 3.5, classes: ["HTML", "CSS", "JavaScript"] },
+  { name: "Kit", GPA: 4, classes: ["HTML", "CSS", "JavaScript", "React"] },
+  { name: "Jorge", GPA: 4, classes: ["HTML", "CSS", "JavaScript", "React"] },
+  { name: "Abdul", GPA: 0.5, classes: ["HTML", "CSS", "JavaScript", "React"] },
+  { name: "Jordan", GPA: 1.5, classes: ["HTML", "CSS", "JavaScript", "React"] },
+  { name: "Dre", GPA: 4, classes: ["HTML", "CSS", "JavaScript", "React"] },
+];
+
+module.exports = studentsGrades;
